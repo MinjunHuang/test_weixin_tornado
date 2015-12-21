9f0fc9c3aeea7bc76833d36e9def7b87894e13bc
chenvista <chenvista@gmail.com>
2015-12-21 15:41:06 +0800

    加入redis连接池配置

--- app.py
+++ app.py
@@ -2,11 +2,23 @@
 from urls import urls
 import tornado.web
 import os
+import redis
 
-SETTINGS = dict(template_path=os.path.join(os.path.dirname(__file__), "templates"), \
-    static_path=os.path.join(os.path.dirname(__file__), "static"),
-)
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = urls
+        
+        
+        #redis连接池
+        pool = redis.ConnectionPool(host='127.0.0.1', port=6379, db=0)
+        self.db = redis.Redis(connection_pool=pool)
 
-app = tornado.web.Application(
-    handlers = urls,**SETTINGS
-)
+        settings = dict(
+            template_path=os.path.join(os.path.dirname(__file__), "templates"), 
+            static_path=os.path.join(os.path.dirname(__file__), "static"),
+            debug=True,
+        )
+
+        tornado.web.Application.__init__(self,handlers,**settings)
+        
+app = Application()
--- server.py
+++ server.py
@@ -1,15 +1,17 @@
 #coding:utf-8
-
 import tornado.ioloop
+import tornado.httpserver
 import sys
 from app import app
+
 
 PORT = '8080'
 ADDR = '0.0.0.0'
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         PORT = sys.argv[1]
-    app.listen(PORT,address=ADDR)
-    print 'Development server is running at http://127.0.0.1:%s/' % PORT
+    http_server = tornado.httpserver.HTTPServer(app)
+    http_server.listen(PORT,address=ADDR)
+    print 'Development server is running at http://%(addr)s:%(port)s/' % {'addr':ADDR,'port':PORT}
     print 'Quit the server with CONTROL-C'
     tornado.ioloop.IOLoop.instance().start()
